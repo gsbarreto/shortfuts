386d827d541851d659a4b2f50e7d103cba2a1442
Joe Martella <martellaj@live.com>
2018-03-31 15:14:49 -0700

    Renders shortcuts in popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import './Popup.scss';
 
 interface AppProps {}
 
 interface AppState {}
 
+type Command = chrome.commands.Command;
+
+@observer
 export default class Popup extends React.Component<AppProps, AppState> {
+    @observable
+    private commands: Map<string, Command> = new Map<string, Command>();
+
     constructor(props: AppProps, state: AppState) {
         super(props, state);
+    }
+
+    componentWillMount() {
+        chrome.commands.getAll((commands: Command[]) => {
+            console.log(commands);
+
+            for (const command of commands) {
+                this.commands.set(command.name, command);
+            }
+        });
     }
 
     componentDidMount() {
@@ -16,10 +34,41 @@ export default class Popup extends React.Component<AppProps, AppState> {
     }
 
     render() {
+        return <div className="popupContainer">{this.renderShortfuts()}</div>;
+    }
+
+    /**
+     * Renders all available shortcuts.
+     */
+    renderShortfuts() {
+        // Don't render anything if commands haven't loaded yet.
+        if (!this.commands) {
+            return <div />;
+        }
+
+        // Build array from map.
+        const commandArray = Array.from(this.commands);
+
         return (
-            <div className="popupContainer">
-                Hello, world!
+            <div className="shortfutsList">
+                {commandArray.map(this.renderShortfut)}
             </div>
-        )
+        );
+    }
+
+    /**
+     * Renders an individual shortcut.
+     */
+    renderShortfut([key, command]: [string, Command]) {
+        return (
+            <div className="shortfut" key={command.name}>
+                <span className="shortfutDescription">
+                    {command.description || 'Open extension popup'}
+                </span>
+                <span className="shortfutShortcut">
+                    {command.shortcut || 'Not set'}
+                </span>
+            </div>
+        );
     }
 }
