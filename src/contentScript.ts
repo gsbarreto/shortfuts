89aa618f5c3cb41b6b92572ed69a53f9fb955082
Joe Martella <martellaj@live.com>
2018-03-31 13:49:14 -0700

    Hooks up configurable command stubs

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -3,9 +3,31 @@ import { log } from './utils/logger';
 (function() {
     log('Running contentScript...');
 
+    function logHotkeyReceived(hotkeyName: string) {
+        log(`${hotkeyName} received in content script!`);
+    }
+
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.buyNow) {
-            log('buyNow hotkey fired!');
+        if (request.toggleExtension) {
+            logHotkeyReceived('toggleExtension');
+        } else if (request.futbin) {
+            logHotkeyReceived('futbin');
+        } else if (request.storeInClub) {
+            logHotkeyReceived('storeInClub');
+        } else if (request.buyNow) {
+            logHotkeyReceived('buyNow');
+        } else if (request.comparePrice) {
+            logHotkeyReceived('comparePrice');
+        } else if (request.quickSell) {
+            logHotkeyReceived('quickSell');
+        } else if (request.sendToTransferList) {
+            logHotkeyReceived('sendToTransferList');
+        } else if (request.listMinBin) {
+            logHotkeyReceived('listMinBin');
+        } else if (request.list) {
+            logHotkeyReceived('list');
+        } else if (request.buyBronzePack) {
+            logHotkeyReceived('buyBronzePack');
         }
     });
 })();
--- src/eventPage.ts
+++ src/eventPage.ts
@@ -14,14 +14,66 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 // Listens for hotkeys to be pressed and notifies content script.
 chrome.commands.onCommand.addListener(command => {
-    log(`Command: ${command}`);
-
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         const currentTab = tabs[0].id;
 
         switch (command) {
-            case 'buyNow': {
+            // toggleExtension
+            case 'command0': {
+                chrome.tabs.sendMessage(currentTab, { toggleExtension: true });
+                break;
+            }
+            // futbin
+            case 'command1': {
+                chrome.tabs.sendMessage(currentTab, { futbin: true });
+                break;
+            }
+            // storeInClub
+            case 'command2': {
+                chrome.tabs.sendMessage(currentTab, { storeInClub: true });
+                break;
+            }
+            // buyNow
+            case 'command3': {
                 chrome.tabs.sendMessage(currentTab, { buyNow: true });
+                break;
+            }
+            // comparePrice
+            case 'command4': {
+                chrome.tabs.sendMessage(currentTab, { comparePrice: true });
+                break;
+            }
+            // quickSell
+            case 'command5': {
+                chrome.tabs.sendMessage(currentTab, { quickSell: true });
+                break;
+            }
+            // sendToTransferList
+            case 'command6': {
+                chrome.tabs.sendMessage(currentTab, {
+                    sendToTransferList: true
+                });
+                break;
+            }
+            // listMinBin
+            case 'command7': {
+                chrome.tabs.sendMessage(currentTab, {
+                    listMinBin: true
+                });
+                break;
+            }
+            // list
+            case 'command8': {
+                chrome.tabs.sendMessage(currentTab, {
+                    list: true
+                });
+                break;
+            }
+            // buyBronzePack
+            case 'command9': {
+                chrome.tabs.sendMessage(currentTab, {
+                    buyBronzePack: true
+                });
                 break;
             }
         }
