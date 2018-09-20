c41e3e86fabb50ee3f7060d0ea612010a5f17efd
Joe Martella <martellaj@live.com>
2018-09-19 21:12:42 -0700

    Fixes store all in club

--- src/provider/storeAllInClub.ts
+++ src/provider/storeAllInClub.ts
@@ -2,7 +2,7 @@ import clickElement from './helpers/clickElement';
 
 export default function storeAllInClub() {
     const button = document.getElementsByClassName(
-        'standard section-header-btn mini call-to-action'
+        'btn-standard section-header-btn mini call-to-action'
     )[0];
 
     clickElement(button);
