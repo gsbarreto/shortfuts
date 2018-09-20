2c6700b14b5681116c1005209f8a8daddb84ff3f
Joe Martella <joem@microsoft.com>
2018-09-20 07:21:42 -0700

    Improves back button shortcut

--- src/provider/back.ts
+++ src/provider/back.ts
@@ -3,23 +3,28 @@ import isUserOnPage from './helpers/isUserOnPage';
 import { logError } from '../utils/logger';
 
 export default function goBack() {
-    const searchResultsHeader = document.getElementsByClassName(
-        'navbar-style-secondary'
-    )[0];
-
-    if (!searchResultsHeader) {
-        logError(
-            `Failed to "go back" because user isn't on the "Search Results" page.`
-        );
+    // Prioritizes back button in secondary nav.
+    const secondaryHeader = document.getElementsByClassName('navbar-style-secondary')[0];
+    if (secondaryHeader && secondaryHeader.getElementsByTagName('button')[0]) {
+        clickElement(secondaryHeader.getElementsByTagName('button')[0]);
         return;
     }
 
-    try {
-        const backButton = searchResultsHeader.getElementsByTagName(
-            'button'
-        )[0];
-        clickElement(backButton);
-    } catch (error) {
-        logError(error);
+    // Falls back to primary nav back button.
+    if (
+        isUserOnPage('Search Results') ||
+        isUserOnPage('Objectives') ||
+        isUserOnPage('Search the Transfer Market') ||
+        isUserOnPage('Transfer Targets') ||
+        isUserOnPage('Transfers') ||
+        isUserOnPage('Squad Building Challenges') ||
+        isUserOnPage('Squad Management') ||
+        isUserOnPage('Players') ||
+        isUserOnPage('Consumables') ||
+        isUserOnPage('Staff') ||
+        isUserOnPage('Club Items')
+    ) {
+        clickElement(document.getElementsByClassName('btn-navigation')[0]);
+        return;
     }
 }
