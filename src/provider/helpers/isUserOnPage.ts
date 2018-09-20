b57eb4a4f43787529a954b44585b28308a7f0e71
Joe Martella <martellaj@live.com>
2018-09-19 18:46:33 -0700

    Fixes isUserOnPage

--- src/provider/helpers/isUserOnPage.ts
+++ src/provider/helpers/isUserOnPage.ts
@@ -2,6 +2,6 @@
  * Checks if user is on specific page.
  */
 export default function isUserOnPage(pageTitle: string) {
-    const title = document.getElementById('futHeaderTitle');
+    const title = document.getElementsByClassName('title')[0];
     return title && title.innerHTML === pageTitle;
 }
