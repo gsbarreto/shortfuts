951012e6ade9d7c732feae314d3a76035c901dec
Joe Martella <martellaj@live.com>
2018-09-27 21:36:19 -0700

    Adds shortcut validation

--- src/shortcuts/EditShortcuts.tsx
+++ src/shortcuts/EditShortcuts.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { ITextField, TextField } from 'office-ui-fabric-react/lib/TextField';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import './EditShortcuts.scss';
 
 @observer
 export default class EditShortcuts extends React.Component<{}, {}> {
-    // @observable private isNativeShortcuts = true;
+    private usedShortcuts: {} = {};
+
+    private backTextField: ITextField;
+    private bidTextField: ITextField;
+
+    @observable private hasError = false;
 
     componentWillMount() {
         // chrome.storage.sync.get('isNativeShortcuts', data => {
@@ -25,22 +30,65 @@ export default class EditShortcuts extends React.Component<{}, {}> {
     render() {
         return (
             <div className="editShortcutsContainer ms-Fabric">
-                <TextField label="Go back (on any page)" />
-                <TextField label="Bid on card" />
+                <TextField label="Go back (on any page)" componentRef={ (ref) => this.backTextField = ref } />
+                <TextField label="Bid on card" componentRef={ (ref) => this.bidTextField = ref } />
+                { this.hasError && <div className="editShortcutsError">All shortcuts must be a single character and unique.</div> }
                 <div className="editShortcutsButtons">
-                    <PrimaryButton>Save</PrimaryButton>
+                    <PrimaryButton onClick={ this.onSaveShortcutsClicked }>Save</PrimaryButton>
                     <DefaultButton>Discard</DefaultButton>
                 </div>
             </div>
         );
     }
 
-    private onSaveShortcuts = () => {
+    private onSaveShortcutsClicked = () => {
         // chrome.storage.sync.set(
         //     { isNativeShortcuts: isNativeShortcuts },
         //     () => {
         //         this.isNativeShortcuts = isNativeShortcuts;
         //     }
         // );
+
+        // Determine if there's an error (shortcuts are invalid).
+        this.hasError = !this.validateShortcuts();
+
+        // If there's an error, don't save shortcuts.
+        if (this.hasError) {
+            return;
+        }
     };
+
+    private validateShortcuts = (): boolean => {
+        // Clear used shortcuts map (from last validation);
+        this.usedShortcuts = {};
+
+        if (this.backTextField.value && !this.validateShortcut(this.backTextField.value)) {
+            return false;
+        }
+
+        if (this.bidTextField.value && !this.validateShortcut(this.bidTextField.value)) {
+            return false;
+        }
+
+        // If nothing short-circuited, it means all shortcuts are valid.
+        return true;
+    }
+
+    private validateShortcut = (shortcut: string): boolean => {
+        // Shortcut has to be 1 character.
+        if (shortcut.length !== 1) {
+            return false;
+        }
+
+        // Shortcuts must be unique.
+        if (this.usedShortcuts[shortcut]) {
+            return false;
+        }
+
+        // Mark shortcut as used.
+        this.usedShortcuts[shortcut] = true;
+
+        // Shortcut is valid.
+        return true;
+    }
 }
