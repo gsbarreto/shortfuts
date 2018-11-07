aaf29666ccdb9490edeb6191fcb93e4bc9322f64
Joe Martella <joem@microsoft.com>
2018-11-07 07:39:25 -0800

    Adds better GA

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -7,191 +7,170 @@ import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { log } from './utils/logger';
 
 (function() {
-  log("Content script has loaded and is running.");
+    log('Content script has loaded and is running.');
 
-  // Initialize OUFR icons.
-  initializeIcons();
+    // Initialize OUFR icons.
+    initializeIcons();
 
-  setTimeout(() => {
-    const header = document.getElementById("FIFAHeader");
-    if (header) {
-      const updateAnchor = document.createElement("div");
-      updateAnchor.id = "updateAnchor";
-      header.appendChild(updateAnchor);
+    setTimeout(() => {
+        const header = document.getElementById('FIFAHeader');
+        if (header) {
+            const updateAnchor = document.createElement('div');
+            updateAnchor.id = 'updateAnchor';
+            header.appendChild(updateAnchor);
 
-      const a = document.createElement("a");
-      a.innerText = "Enjoy shortfuts? Please consider buying me a coffee!";
-      a.style.display = "inline-block";
-      a.style.paddingLeft = "24px";
-      a.style.paddingTop = "4px";
-      a.style.fontSize = "30px";
-      a.target = "_blank";
-      a.href = "https://ko-fi.com/shortfuts";
-      header.appendChild(a);
+            const a = document.createElement('a');
+            a.innerText = 'Enjoy shortfuts? Please consider buying me a coffee!';
+            a.style.display = 'inline-block';
+            a.style.paddingLeft = '24px';
+            a.style.paddingTop = '4px';
+            a.style.fontSize = '30px';
+            a.target = '_blank';
+            a.href = 'https://ko-fi.com/shortfuts';
+            header.appendChild(a);
 
-      ReactDOM.render(
-        <Announcement />,
-        document.getElementById("updateAnchor")
-      );
+            ReactDOM.render(<Announcement />, document.getElementById('updateAnchor'));
 
-      return;
-    }
-  }, 2000);
-
-  // // Update badge with current status.
-  // chrome.storage.sync.get("isActive", data => {
-  //   // Make extension active on first run.
-  //   if (data.isActive === undefined) {
-  //     chrome.storage.sync.set({ isActive: true }, () => {
-  //       chrome.runtime.sendMessage({ isActive: true });
-  //     });
-  //     return;
-  //   }
-
-  //   chrome.runtime.sendMessage({ isActive: data.isActive });
-  // });
-
-  // If there are no shortcuts saved, set them as the default ones.
-  chrome.storage.sync.get("shortcutsMap", data => {
-    if (!data.shortcutsMap) {
-      const defaultShortcuts = {
-        D: Shortcut.BID,
-        N: Shortcut.BIN,
-        B: Shortcut.BRONZE_PACK,
-        C: Shortcut.COMPARE,
-        J: Shortcut.DECREASE_MAX,
-        I: Shortcut.DECREASE_MIN,
-        K: Shortcut.INCREASE_MAX,
-        O: Shortcut.INCREASE_MIN,
-        M: Shortcut.LIST_MIN_BIN,
-        L: Shortcut.LIST,
-        A: Shortcut.QUICK_SELL_ALL,
-        Q: Shortcut.QUICK_SELL,
-        P: Shortcut.SEARCH,
-        X: Shortcut.STORE_ALL,
-        S: Shortcut.STORE,
-        W: Shortcut.TOGGLE_WATCH,
-        T: Shortcut.TRANSFER_LIST
-      };
-
-      chrome.storage.sync.set({ shortcutsMap: defaultShortcuts }, () => {
-        log("Default shortcuts set for this user.");
-      });
-    }
-  });
-
-  // Get provider that performs hotkey actions for correct version of web app.
-  const provider = getProvider();
-
-  // Sets up listeners for non-configurable shortcuts.
-  window.addEventListener("keydown", ev => {
-    const keyCode = ev.keyCode;
-
-    // If user is typing in an input, ignore hotkeys.
-    if (document.activeElement.tagName.toLocaleLowerCase() === "input") {
-      return;
-    }
-
-    chrome.storage.sync.get("isActive", data => {
-      // // Update badge with current status.
-      // const isActive = data.isActive;
-      // chrome.runtime.sendMessage({ isActive: isActive });
-
-      // /**
-      //  * Check "Alt + Space" first because that toggles the extension as active
-      //  * or not.
-      //  */
-      // if (ev.altKey && keyCode === 32 /* Alt + Space */) {
-      //   chrome.storage.sync.set({ isActive: !isActive }, function() {
-      //     chrome.runtime.sendMessage({
-      //       isActive: !isActive
-      //     });
-      //   });
-      //   return;
-      // }
-
-      // // If extension isn't active, don't process hotkeys.
-      // if (!isActive) {
-      //   return;
-      // }
-
-      switch (keyCode) {
-        case 8 /* backspace */:
-          provider.back();
-          return;
-        case 38 /* up arrow */:
-          provider.move("up");
-          return;
-        case 40 /* down arrow */:
-          provider.move("down");
-          return;
-        case 37 /* left arrow */:
-          provider.pagePrevious();
-          return;
-        case 39 /* right arrow */:
-          provider.pageNext();
-          return;
-      }
-
-      chrome.storage.sync.get("shortcutsMap", data => {
-        const shortcutsMap = data.shortcutsMap;
-        const shortcutKey = String.fromCharCode(keyCode);
-        const shortcut = shortcutsMap[shortcutKey];
-
-        switch (shortcut) {
-          case Shortcut.BID:
-            provider.makeBid();
-            break;
-          case Shortcut.BIN:
-            provider.buyNow();
-            break;
-          case Shortcut.BRONZE_PACK:
-            provider.buyBronzePack();
-            break;
-          case Shortcut.COMPARE:
-            provider.comparePrice();
-            break;
-          case Shortcut.DECREASE_MAX:
-            provider.decreaseMaxBidPrice();
-            break;
-          case Shortcut.DECREASE_MIN:
-            provider.decreaseMinBidPrice();
-            break;
-          case Shortcut.INCREASE_MAX:
-            provider.increaseMaxBidPrice();
-            break;
-          case Shortcut.INCREASE_MIN:
-            provider.increaseMinBidPrice();
-            break;
-          case Shortcut.LIST_MIN_BIN:
-            provider.listMinBin();
-            break;
-          case Shortcut.LIST:
-            provider.list();
-            break;
-          case Shortcut.QUICK_SELL_ALL:
-            provider.quickSellAll();
-            break;
-          case Shortcut.QUICK_SELL:
-            provider.quickSell();
-            break;
-          case Shortcut.SEARCH:
-            provider.search();
-            break;
-          case Shortcut.STORE_ALL:
-            provider.storeAllInClub();
-            break;
-          case Shortcut.STORE:
-            provider.storeInClub();
-            break;
-          case Shortcut.TOGGLE_WATCH:
-            provider.watch();
-            break;
-          case Shortcut.TRANSFER_LIST:
-            provider.sendToTransferList();
-            break;
+            return;
         }
-      });
+    }, 2000);
+
+    // If there are no shortcuts saved, set them as the default ones.
+    chrome.storage.sync.get('shortcutsMap', data => {
+        if (!data.shortcutsMap) {
+            const defaultShortcuts = {
+                D: Shortcut.BID,
+                N: Shortcut.BIN,
+                B: Shortcut.BRONZE_PACK,
+                C: Shortcut.COMPARE,
+                J: Shortcut.DECREASE_MAX,
+                I: Shortcut.DECREASE_MIN,
+                K: Shortcut.INCREASE_MAX,
+                O: Shortcut.INCREASE_MIN,
+                M: Shortcut.LIST_MIN_BIN,
+                L: Shortcut.LIST,
+                A: Shortcut.QUICK_SELL_ALL,
+                Q: Shortcut.QUICK_SELL,
+                P: Shortcut.SEARCH,
+                X: Shortcut.STORE_ALL,
+                S: Shortcut.STORE,
+                W: Shortcut.TOGGLE_WATCH,
+                T: Shortcut.TRANSFER_LIST,
+            };
+
+            chrome.storage.sync.set({ shortcutsMap: defaultShortcuts }, () => {
+                log('Default shortcuts set for this user.');
+            });
+        }
     });
-  });
+
+    // Get provider that performs hotkey actions for correct version of web app.
+    const provider = getProvider();
+
+    // Sets up listeners for non-configurable shortcuts.
+    window.addEventListener('keydown', ev => {
+        const keyCode = ev.keyCode;
+
+        // If user is typing in an input, ignore hotkeys.
+        if (document.activeElement.tagName.toLocaleLowerCase() === 'input') {
+            return;
+        }
+
+        chrome.storage.sync.get('isActive', data => {
+            switch (keyCode) {
+                case 8 /* backspace */:
+                    provider.back();
+                    return;
+                case 38 /* up arrow */:
+                    provider.move('up');
+                    return;
+                case 40 /* down arrow */:
+                    provider.move('down');
+                    return;
+                case 37 /* left arrow */:
+                    provider.pagePrevious();
+                    return;
+                case 39 /* right arrow */:
+                    provider.pageNext();
+                    return;
+            }
+
+            chrome.storage.sync.get('shortcutsMap', data => {
+                const shortcutsMap = data.shortcutsMap;
+                const shortcutKey = String.fromCharCode(keyCode);
+                const shortcut = shortcutsMap[shortcutKey];
+
+                switch (shortcut) {
+                    case Shortcut.BID:
+                        provider.makeBid();
+                        chrome.runtime.sendMessage({ makeBid: true });
+                        break;
+                    case Shortcut.BIN:
+                        provider.buyNow();
+                        chrome.runtime.sendMessage({ buyNow: true });
+                        break;
+                    case Shortcut.BRONZE_PACK:
+                        provider.buyBronzePack();
+                        chrome.runtime.sendMessage({ buyBronzePack: true });
+                        break;
+                    case Shortcut.COMPARE:
+                        provider.comparePrice();
+                        chrome.runtime.sendMessage({ comparePrice: true });
+                        break;
+                    case Shortcut.DECREASE_MAX:
+                        provider.decreaseMaxBidPrice();
+                        chrome.runtime.sendMessage({ decreaseMaxBidPrice: true });
+                        break;
+                    case Shortcut.DECREASE_MIN:
+                        provider.decreaseMinBidPrice();
+                        chrome.runtime.sendMessage({ decreaseMinBidPrice: true });
+                        break;
+                    case Shortcut.INCREASE_MAX:
+                        provider.increaseMaxBidPrice();
+                        chrome.runtime.sendMessage({ increaseMaxBidPrice: true });
+                        break;
+                    case Shortcut.INCREASE_MIN:
+                        provider.increaseMinBidPrice();
+                        chrome.runtime.sendMessage({ increaseMinBidPrice: true });
+                        break;
+                    case Shortcut.LIST_MIN_BIN:
+                        provider.listMinBin();
+                        chrome.runtime.sendMessage({ listMinBin: true });
+                        break;
+                    case Shortcut.LIST:
+                        provider.list();
+                        chrome.runtime.sendMessage({ list: true });
+                        break;
+                    case Shortcut.QUICK_SELL_ALL:
+                        provider.quickSellAll();
+                        chrome.runtime.sendMessage({ quickSellAll: true });
+                        break;
+                    case Shortcut.QUICK_SELL:
+                        provider.quickSell();
+                        chrome.runtime.sendMessage({ quickSell: true });
+                        break;
+                    case Shortcut.SEARCH:
+                        provider.search();
+                        chrome.runtime.sendMessage({ search: true });
+                        break;
+                    case Shortcut.STORE_ALL:
+                        provider.storeAllInClub();
+                        chrome.runtime.sendMessage({ storeAllInClub: true });
+                        break;
+                    case Shortcut.STORE:
+                        provider.storeInClub();
+                        chrome.runtime.sendMessage({ storeInClub: true });
+                        break;
+                    case Shortcut.TOGGLE_WATCH:
+                        provider.watch();
+                        chrome.runtime.sendMessage({ watch: true });
+                        break;
+                    case Shortcut.TRANSFER_LIST:
+                        provider.sendToTransferList();
+                        chrome.runtime.sendMessage({ sendToTransferList: true });
+                        break;
+                }
+            });
+        });
+    });
 })();
--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,81 +1,111 @@
 // Google Analytics bootstrap
 var _gaq = _gaq || [];
-_gaq.push(["_setAccount", "UA-108017342-2"]);
-var ga = document.createElement("script");
-ga.type = "text/javascript";
-ga.async = true;
-ga.src = "https://ssl.google-analytics.com/ga.js";
-var s = document.getElementsByTagName("script")[0];
-s.parentNode.insertBefore(ga, s);
+_gaq.push(['_setAccount', 'UA-108017342-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
 
 chrome.browserAction.setBadgeBackgroundColor({
-  color: "#0078d4"
+    color: '#0078d4',
 });
 chrome.browserAction.setBadgeText({
-  text: "ON"
+    text: 'ON',
 });
 
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // onMessage must return "true" if response is async.
-  let isResponseAsync = false;
+    // onMessage must return "true" if response is async.
+    let isResponseAsync = false;
 
-  if (request.contactDeveloper) {
-    contactDeveloper();
-  } else if (request.review) {
-    review();
-  } // else if (request.isActive !== undefined) {
-  //   chrome.browserAction.setBadgeBackgroundColor({
-  //     color: "#0078d4"
-  //   });
-  //   chrome.browserAction.setBadgeText({
-  //     text: request.isActive ? "ON" : "OFF"
-  //   });
-  // }
+    if (request.contactDeveloper) {
+        contactDeveloper();
+    } else if (request.review) {
+        review();
+    } else if (request.makeBid) {
+        _gaq.push(['_trackEvent', 'telemetry', 'makeBid']);
+    } else if (request.buyNow) {
+        _gaq.push(['_trackEvent', 'telemetry', 'buyNow']);
+    } else if (request.buyBronzePack) {
+        _gaq.push(['_trackEvent', 'telemetry', 'buyBronzePack']);
+    } else if (request.comparePrice) {
+        _gaq.push(['_trackEvent', 'telemetry', 'comparePrice']);
+    } else if (request.decreaseMaxBidPrice) {
+        _gaq.push(['_trackEvent', 'telemetry', 'decreaseMaxBidPrice']);
+    } else if (request.decreaseMinBidPrice) {
+        _gaq.push(['_trackEvent', 'telemetry', 'decreaseMinBidPrice']);
+    } else if (request.increaseMaxBidPrice) {
+        _gaq.push(['_trackEvent', 'telemetry', 'increaseMaxBidPrice']);
+    } else if (request.increaseMinBidPrice) {
+        _gaq.push(['_trackEvent', 'telemetry', 'increaseMinBidPrice']);
+    } else if (request.listMinBin) {
+        _gaq.push(['_trackEvent', 'telemetry', 'listMinBin']);
+    } else if (request.list) {
+        _gaq.push(['_trackEvent', 'telemetry', 'list']);
+    } else if (request.quickSellAll) {
+        _gaq.push(['_trackEvent', 'telemetry', 'quickSellAll']);
+    } else if (request.quickSell) {
+        _gaq.push(['_trackEvent', 'telemetry', 'quickSell']);
+    } else if (request.search) {
+        _gaq.push(['_trackEvent', 'telemetry', 'search']);
+    } else if (request.storeAllInClub) {
+        _gaq.push(['_trackEvent', 'telemetry', 'storeAllInClub']);
+    } else if (request.storeInClub) {
+        _gaq.push(['_trackEvent', 'telemetry', 'storeInClub']);
+    } else if (request.watch) {
+        _gaq.push(['_trackEvent', 'telemetry', 'watch']);
+    } else if (request.sendToTransferList) {
+        _gaq.push(['_trackEvent', 'telemetry', 'sendToTransferList']);
+    }
 
-  return isResponseAsync;
+    return isResponseAsync;
 });
 
 /**
  * This function hits the mailto protocol and returns user to current tab.
  */
 function contactDeveloper() {
-  chrome.tabs.query(
-    {
-      active: true
-    },
-    tabs => {
-      // Caches current user tab so we can return to it.
-      const currentTabId = tabs[0].id;
-
-      chrome.tabs.create(
+    chrome.tabs.query(
         {
-          active: true,
-          url: "mailto:shortfuts@gmail.com?subject=shortfuts%20feedback"
+            active: true,
         },
-        mailToTab => {
-          setTimeout(function() {
-            // Closes tab created by mailto protocol.
-            chrome.tabs.remove(mailToTab.id);
+        tabs => {
+            // Caches current user tab so we can return to it.
+            const currentTabId = tabs[0].id;
 
-            // Makes previously focused tab selected.
-            chrome.tabs.update(currentTabId, {
-              highlighted: true
-            });
-          }, 150);
+            chrome.tabs.create(
+                {
+                    active: true,
+                    url: 'mailto:shortfuts@gmail.com?subject=shortfuts%20feedback',
+                },
+                mailToTab => {
+                    setTimeout(function() {
+                        // Closes tab created by mailto protocol.
+                        chrome.tabs.remove(mailToTab.id);
+
+                        // Makes previously focused tab selected.
+                        chrome.tabs.update(currentTabId, {
+                            highlighted: true,
+                        });
+                    }, 150);
+                }
+            );
         }
-      );
-    }
-  );
+    );
 }
 
 /**
  * Opens Chrome Web Store page so user can review.
  */
 function review() {
-  chrome.tabs.create({
-    active: true,
-    url:
-      "https://chrome.google.com/webstore/detail/shortfuts/piepdojghinggmddebidfkhfbdaggnmh"
-  });
+    chrome.tabs.create({
+        active: true,
+        url: 'https://chrome.google.com/webstore/detail/shortfuts/piepdojghinggmddebidfkhfbdaggnmh',
+    });
 }
