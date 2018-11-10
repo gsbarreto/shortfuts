758d4848dadff8e15c8144de928347ad3f105b48
Joe Martella <martellaj@live.com>
2018-11-09 20:15:01 -0800

    Fixes getListItems for non-English

--- src/provider/helpers/getListItems.ts
+++ src/provider/helpers/getListItems.ts
@@ -1,36 +1,23 @@
-import isUserOnPage from './isUserOnPage';
+import isUserOnSearchResultsPage from './isUserOnSearchResultsPage';
+import isUserOnUnassignedPage from './isUserOnUnassignedPage';
 
 export default function getListItems() {
-    let items = [];
+  let items = [];
 
-    if (
-        isUserOnPage('Search Results') ||
-        isUserOnPage('Players') ||
-        isUserOnPage('Club Items') ||
-        isUserOnPage('Training') ||
-        isUserOnPage('Contracts') ||
-        isUserOnPage('Fitness') ||
-        isUserOnPage('Healing') ||
-        isUserOnPage('Positioning') ||
-        isUserOnPage('Chemistry Styles')
-    ) {
-        const itemList = document.getElementsByClassName(
-            'paginated-item-list'
-        )[0];
-        items = Array.from(itemList.getElementsByClassName('listFUTItem'));
-    } else if (isUserOnPage('Transfer List') || isUserOnPage('Unassigned')) {
-        const itemLists = Array.from(
-            document.getElementsByClassName('itemList')
-        );
-        itemLists.forEach(function(itemList) {
-            items = items.concat(
-                Array.from(itemList.getElementsByClassName('listFUTItem'))
-            );
-        }, this);
-    } else {
-        const itemList = document.getElementsByClassName('itemList')[0];
-        items = Array.from(itemList.getElementsByClassName('listFUTItem'));
-    }
+  if (isUserOnSearchResultsPage()) {
+    const itemList = document.getElementsByClassName("paginated-item-list")[0];
+    items = Array.from(itemList.getElementsByClassName("listFUTItem"));
+  } else if (isUserOnUnassignedPage()) {
+    const itemLists = Array.from(document.getElementsByClassName("itemList"));
+    itemLists.forEach(function(itemList) {
+      items = items.concat(
+        Array.from(itemList.getElementsByClassName("listFUTItem"))
+      );
+    }, this);
+  } else {
+    const itemList = document.getElementsByClassName("itemList")[0];
+    items = Array.from(itemList.getElementsByClassName("listFUTItem"));
+  }
 
-    return items;
+  return items;
 }
