ca2b040dc1fd7c91a9c6f4b37013f49048b7798c
Joe Martella <joem@microsoft.com>
2018-11-20 08:10:36 -0800

    Bumps to v5.5.1

--- src/Announcement.tsx
+++ src/Announcement.tsx
@@ -109,7 +109,7 @@ export default class Announcement extends React.Component<{}, {}> {
                 !announcementKillswitchEnabled
             ) {
                 this.setAnnouncement(
-                    `This new update (v5.5.0) includes added support for Portuguese as well as an anti-ban warning (where you'll be warned if you're spamming the transfer market too much).`,
+                    `This new update (v5.5.1) includes added support for Portuguese as well as an anti-ban warning (where you'll be warned if you're spamming the transfer market too much).`,
                     `Please feel free to reach out via email with any bugs or feature requests. Happy FUTing!`
                 );
 
