7b253b4c88ec58e442631ed5f9bf2d9fcd6b0d28
Joe Martella <joem@microsoft.com>
2018-11-25 08:43:15 -0800

    Fixes bug with buy now delay

--- src/provider/buyNow.ts
+++ src/provider/buyNow.ts
@@ -6,10 +6,20 @@ import { logError } from "../utils/logger";
 export default function buyNow() {
     if (isUserOnSearchResultsPage()) {
         try {
-            setTimeout(() => {
-                clickBuyNowButton();
-                confirmConfirmationDialog();
-            }, wait());
+            chrome.storage.sync.get("binDelay", (data: any) => {
+                const shouldWait =
+                    data.binDelay === undefined ? true : data.binDelay;
+
+                if (shouldWait) {
+                    setTimeout(() => {
+                        clickBuyNowButton();
+                        confirmConfirmationDialog();
+                    }, wait());
+                } else {
+                    clickBuyNowButton();
+                    confirmConfirmationDialog();
+                }
+            });
         } catch (error) {
             logError(`Oops! Couldn't "buy it now" for some reason...`);
         }
@@ -19,15 +29,6 @@ export default function buyNow() {
 function wait(): number {
     const min = 350;
     const max = 450;
-
-    let shouldWait = true;
-    chrome.storage.sync.get("binDelay", (data: any) => {
-        shouldWait = data.binDelay === undefined ? true : data.binDelay;
-    });
-
-    if (!shouldWait) {
-        return 0;
-    }
 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
