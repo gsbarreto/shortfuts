2a22c5f4dbf767b0aba8809c83ef6fbcb2d43aa8
Joe Martella <joem@microsoft.com>
2019-01-24 07:42:12 -0800

    Removes "Review" button from announcement so haters can't hate as easily

--- src/popup/Footer.tsx
+++ src/popup/Footer.tsx
@@ -1,35 +1,23 @@
-import * as React from 'react';
-import { ActionButton } from 'office-ui-fabric-react/lib/Button';
-import './Footer.scss';
+import * as React from "react";
+import { ActionButton } from "office-ui-fabric-react/lib/Button";
+import "./Footer.scss";
 
 export default class Footer extends React.Component<{}, {}> {
-  render() {
-    return (
-      <div className="footer ms-bgColor-themeLighterAlt">
-        {/* Review button */}
-        <ActionButton
-          iconProps={{ iconName: "FavoriteStarFill" }}
-          onClick={() => {
-            chrome.runtime.sendMessage({
-              review: true
-            });
-          }}
-        >
-          Review
-        </ActionButton>
-
-        {/* Contact button */}
-        <ActionButton
-          iconProps={{ iconName: "Mail" }}
-          onClick={() => {
-            chrome.runtime.sendMessage({
-              contactDeveloper: true
-            });
-          }}
-        >
-          Contact developer
-        </ActionButton>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="footer ms-bgColor-themeLighterAlt">
+                {/* Contact button */}
+                <ActionButton
+                    iconProps={{ iconName: "Mail" }}
+                    onClick={() => {
+                        chrome.runtime.sendMessage({
+                            contactDeveloper: true
+                        });
+                    }}
+                >
+                    Contact developer
+                </ActionButton>
+            </div>
+        );
+    }
 }
