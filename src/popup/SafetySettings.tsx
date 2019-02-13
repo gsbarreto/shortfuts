9b08df330d3b6e0d5a7d8531c0afdb61129b9d9b
Joe Martella <martellaj@live.com>
2019-02-13 07:08:32 -0800

    Adds premium users

--- src/popup/SafetySettings.tsx
+++ src/popup/SafetySettings.tsx
@@ -108,7 +108,9 @@ export default class SafetySettings extends React.Component<
                 "t.andresen97@gmail.com",
                 "florisvanderhout@gmail.com",
                 "Karolkurpan@gmail.com",
-                "lassepetty@hotmail.no"
+                "lassepetty@hotmail.no",
+                'shekomostafa3939@gmail.com',
+                'iwageehh@gmail.com'
             ];
 
             const lowerCasePaidUsers = paidUsers.map((email: string) =>
