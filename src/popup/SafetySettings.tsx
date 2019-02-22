6022c36727072d1cd1efd11a732e0d05cfa97924
Joe Martella <joem@microsoft.com>
2019-02-22 09:48:37 -0800

    Adds premium user

--- src/popup/SafetySettings.tsx
+++ src/popup/SafetySettings.tsx
@@ -116,7 +116,8 @@ export default class SafetySettings extends React.Component<
                 "ricardo.daroc@gmail.com",
                 "taboubi.tayeb@gmail.com",
                 "zacky5792kid@gmail.com",
-                "48hrs@web.de"
+                "48hrs@web.de",
+                "fraser@mcknespiey.com"
             ];
 
             const lowerCasePaidUsers = paidUsers.map((email: string) =>
